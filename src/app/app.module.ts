f760db87e6345d5be2693119e0981e04659fc0d9
Ritesh Sharma <sharmaritesh3312@gmail.com>
2020-02-12 15:55:10 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { MarkerService } from './marker.service';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MapComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [
+    MarkerService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
